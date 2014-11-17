f8f07f0e49da240f6de64ce2ab4c3d17a3de8623
Gary Horner <groo79@fullsail.edu>
2014-11-16 19:06:59 -0500

    added experiment 5 export
    added the actor and attached animations from Experiment 5

---
+++ Horner_Gareld_Lab4/Assets/Scripts/ActorStateMachine.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActorStateMachine : MonoBehaviour
+{
+
+	Animator anim;
+	private float speed = 1.0f;
+	[SerializeField]
+	private float maxDeltaVel = 10.0f;
+
+		// Use this for initialization
+		void Start ()
+		{
+				anim = GetComponent<Animator> ();
+				if (anim == null) {
+						Debug.Log ("animator not assigned to actor");
+				}
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+		float h = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+		anim.SetFloat ("Speed", y);
+		anim.SetFloat ("Direction", h);
+
+		transform.Rotate (0, h, 0);
+
+		Vector3 targetDirection = transform.forward * y;
+		Vector3 targetVelocity = targetDirection * speed;
+		Vector3 deltaVelocity = targetVelocity - rigidbody.velocity;
+
+		deltaVelocity.x = Mathf.Clamp (deltaVelocity.x, -maxDeltaVel, maxDeltaVel);
+		deltaVelocity.z = Mathf.Clamp (deltaVelocity.z, -maxDeltaVel, maxDeltaVel);
+		deltaVelocity.y = Mathf.Clamp (deltaVelocity.y, 0, 0);
+
+		rigidbody.AddForce (deltaVelocity, ForceMode.VelocityChange);
+
+		}
+}
---
+++ Horner_Gareld_Lab4/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+	/*
+	public Vector3 offset;
+	public Transform target;
+	public float rotateSpeed = 5;//*/
+
+	[SerializeField]
+	private float distanceUp;
+	[SerializeField]
+	private float distanceAway;
+	[SerializeField]
+	private float smooth;
+	[SerializeField]
+	private Transform follow;
+
+	private Vector3 targetPosition;
+	
+	void Start() 
+	{
+		//offset = transform.position - target.transform.position;
+	}
+
+	void LateUpdate()
+	{
+
+
+		targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
+		Debug.DrawRay ( follow.position, follow.up * distanceUp, Color.red );
+		Debug.DrawRay ( follow.position, -1f * follow.forward * distanceAway, Color.blue );
+		Debug.DrawLine ( follow.position, targetPosition, Color.magenta );
+
+		transform.position = Vector3.Lerp ( transform.position, targetPosition, Time.deltaTime * smooth );
+
+		transform.LookAt( follow );
+	}
+	
+	/*
+	void LateUpdate() 
+	{
+		float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+		target.transform.Rotate(0, horizontal, 0);
+		
+		float desiredAngle = target.transform.eulerAngles.y;
+		Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+		transform.position = target.transform.position - (rotation * offset);
+		
+		transform.LookAt(target.transform);
+	}//*/
+}
