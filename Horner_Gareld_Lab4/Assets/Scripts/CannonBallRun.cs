3af81ee955558ab6e47c49d1e00874fc282a2ad9
AaronBarnard <sinrae_kinesti@yahoo.com>
2014-11-18 20:52:42 -0500

    suppository implant
    bet ya like that

---
+++ Horner_Gareld_Lab4/Assets/Scripts/CannonBallRun.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallisticVelocity : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+//		rigidbody.AddForce(transform.forward  force, ForceMode.Impulse);
+//		rigidbody.AddForce(transform.up  force, ForceMode.Impulse);
+//		rigidbody.AddTorque(torque, ForceMode.Force);
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		shotTime += Time.deltaTime;
+		if(shotTime >= fireRate)
+		{
+			if(!(name.GetComponent("Tower") as Tower).canAim)
+			{
+				GameObject cannonBall = Instantiate(mortar, gunTip.position, Quaternion.identity) as GameObject;
+				cannonBall.rigidbody.velocity = BallisticVelocity(target, shootAngle);
+				Destroy(cannonBall, 10); // cannonball disappears after 10 seconds
+				shotTime = 0.0f;
+			}
+		}
+	}
+	Vector3 BallisticVelocity(Transform target, float angle)
+	{
+		Vector3 dir = target.position - gunTip.position; // get Target Direction
+		float height = dir.y; // get height difference
+		dir.y = 0; // retain only the horizontal direction
+		float dist = dir.magnitude; // get horizontal distance
+		float a = angle * Mathf.Deg2Rad; // Convert angle to radians
+		dir.y = dist * Mathf.Tan(a); // set dir to the elevation angle.
+		dist += height / Mathf.Tan(a); // Correction for small height differences
+		// Calculate the velocity magnitude
+		float velocity = Mathf.Sqrt(dist * Physics.gravity.magnitude / Mathf.Sin(2 * a));
+		return velocity * dir.normalized; // Return the velocity vector.
+
+	}
+	
+
+}
--- Horner_Gareld_Lab4/Assets/Scripts/Melee.cs
+++ Horner_Gareld_Lab4/Assets/Scripts/Melee.cs
@@ -3,13 +3,32 @@ using System.Collections;
 
 public class Melee : MonoBehaviour {
 
+	int damageDealt = 50;
+	
+	 float distance;
+	
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () { 
 	
 	}
+
+	if(Input.GetKeyDown(KeyCode.alpha1) {
+
+		var hit : RaycastHit;
+
+		if (Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), hit)){
+
+			Distance = hit.distance;
+
+			hit.Transform.SendMessage("ApplyDamage",damageDealt, SendMessageOptions.DontRequireReciever);
+		}
+
+	}
 }
+ 
